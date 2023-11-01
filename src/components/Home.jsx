319feeb5c0d93afb74f0f0dd2ded43255fc067e5
Mhedii <mehedi1234.mh@gmail.com>
2023-11-01 12:19:57 +0600

    Delete Functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     { id: "image-10", src: "/images/image-10.jpeg" },
     { id: "image-11", src: "/images/image-11.jpeg" },
   ]);
-
+  const [selectedImages, setSelectedImages] = useState([]);
   const handleDraggingStart = (e, index) => {
     e.dataTransfer.setData("text/plain", index);
   };
@@ -34,15 +34,38 @@ const Home = () => {
     e.preventDefault();
   };
 
+  const handleCheckboxChange = (imageId) => {
+    setSelectedImages((prevSelectedImages) => {
+      if (prevSelectedImages.includes(imageId)) {
+        return prevSelectedImages.filter((id) => id !== imageId);
+      } else {
+        return [...prevSelectedImages, imageId];
+      }
+    });
+  };
+
+  const handleDeleteSelected = () => {
+    const updatedImages = images.filter(
+      (image) => !selectedImages.includes(image.id)
+    );
+    setImages(updatedImages);
+    setSelectedImages([]);
+  };
+
   return (
     <div>
       <div className="h-screen">
         <div className=" px-10 flex justify-between">
           <div className="flex">
             <input type="checkbox" />
-            <h1 className="text-xl font-bold ps-2">3 Files Selected</h1>
+            <h1 className="text-xl font-bold ps-2">
+              {selectedImages.length} Files Selected
+            </h1>
           </div>
-          <div className="text-xl font-semibold  hover:cursor-pointer text-red-500">
+          <div
+            className="text-xl font-semibold  hover:cursor-pointer text-red-500"
+            onClick={handleDeleteSelected}
+          >
             Delete Files
           </div>
         </div>
@@ -51,152 +74,38 @@ const Home = () => {
           {images.map((image, index) => (
             <div
               key={image.id}
-              className={`group relative ${
-                index === 0
-                  ? "hover:bg-slate-100 rounded-lg border border-slate-400 col-span-2 row-span-2"
-                  : "hover:bg-slate-100 rounded-lg border border-slate-400"
-              }`}
+              className={`group relative hover:bg-slate-100 rounded-lg border border-slate-400 ${
+                index === 0 ? " col-span-2 row-span-2" : ""
+              } ${selectedImages.includes(image.id) ? "bg-slate-100" : ""}`}
               draggable
               onDragStart={(e) => handleDraggingStart(e, index)}
               onDrop={(e) => handleDrop(e, index)}
               onDragOver={(e) => handleDraggingComplete(e)}
             >
-              <input
-                type="checkbox"
-                className="w-4 h-4 absolute hidden ms-2 mt-2 group-hover:block cursor-pointer"
-              />
-              <div className="">
+              <div className="relative">
+                <input
+                  type="checkbox"
+                  className={`w-4 h-4   ms-2 mt-2 group-hover:opacity-100 cursor-pointer ${
+                    selectedImages.includes(image.id)
+                      ? "opacity-100"
+                      : "opacity-0"
+                  }`}
+                  onChange={() => handleCheckboxChange(image.id)}
+                />
+
                 <img
                   src={image.src}
-                  className="rounded-lg hover:cursor-pointer hover:opacity-50 max-h-full max-w-full"
+                  className={`rounded-lg hover:cursor-pointer hover:opacity-50 hover:bg-slate-100 max-h-full max-w-full ${
+                    selectedImages.includes(image.id)
+                      ? "opacity-50 bg-slate-100"
+                      : ""
+                  }`}
                   alt=""
                 />
               </div>
             </div>
           ))}
 
-          {/* <div className="group relative hover:bg-slate-100 rounded-lg border border-slate-400 col-span-2 row-span-2">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 absolute  hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-
-            <img
-              src="/public/images/image-1.webp"
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-2.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-3.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-4.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-5.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-6.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 rounded-lg  absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-7.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className="w-4 h-4  absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-8.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className="w-4 h-4  absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-9.webp"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className=" w-4 h-4 absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-10.jpeg"
-              alt=""
-            />
-          </div>
-          <div className="group hover:bg-slate-100 rounded-lg border border-slate-400">
-            <input
-              type="checkbox"
-              className="w-4 h-4  absolute hidden ms-2 mt-2  group-hover:block cursor-pointer "
-            />
-            <img
-              className=" rounded-lg hover:cursor-pointer hover:opacity-50  max-h-full max-w-full "
-              src="/public/images/image-11.jpeg"
-              alt=""
-            />
-          </div> */}
           <div className="relative bg-gray-300 group hover:bg-slate-100 rounded-lg border border-dashed border-black  ">
             <input
               type="file"
